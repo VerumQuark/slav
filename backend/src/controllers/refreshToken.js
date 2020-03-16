30c99e1c8ffb0e5d37a46eab8f97681bb6c60a00
VerumQuark <chichukko@gmail.com>
2020-03-16 17:32:38 +0200

    Create: signUp forms, Refactor: FieldArray

---
+++ backend/src/controllers/refreshToken.js
@@ -0,0 +1,33 @@
+import { RefreshToken } from '../models';
+
+import Controller          from '../utils/controller';
+import { BadRequestError } from '../utils/errors';
+
+class RefreshTokenController {
+
+  constructor () {
+    this._controller = new Controller( RefreshToken );
+  }
+
+  createRefreshToken = async (req, res, next) => {
+    try {
+      const refreshToken = await this._controller.create( {
+                                                            refreshToken: req.tokenPair.value,
+                                                            userId: req.user.id,
+                                                          } );
+
+      if (refreshToken) {
+        return res.send( {
+                           tokenPair: req.tokenPair,
+                           user: req.user,
+                         } );
+      }
+      next( new BadRequestError() );
+    } catch (e) {
+      next( e );
+    }
+  };
+
+}
+
+export default new RefreshTokenController();
